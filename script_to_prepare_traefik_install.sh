dd3a1722cd5e66d9d2c64a3a8d6ccccacf06d38e
Joe Depew <51305256+jdepew88@users.noreply.github.com>
2024-07-08 02:26:48 -0700

    Update script_to_prepare_traefik_install.sh

--- script_to_prepare_traefik_install.sh
+++ script_to_prepare_traefik_install.sh
@@ -13,10 +13,6 @@ echo "Creating acme.json file and setting permissions..."
 touch /opt/appdata/traefik/acme.json
 chmod 600 /opt/appdata/traefik/acme.json
 
-# Open nano text editor to create the docker-compose file
-echo "Opening nano to create the docker-compose.yml file..."
-nano /opt/appdata/traefik/docker-compose.yml
-
-echo "Traefik initial setup for Docker Compose completed."
+echo "acme.json file created and permissions set."
 
 echo "Credit: This script is based on instructions from IBRACORP. For more details, visit: https://docs.ibracorp.io/traefik/master/docker-compose"
